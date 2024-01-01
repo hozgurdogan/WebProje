dec1c3f5b8a1ad0d8aeeb18f1fc2e6576f482cc5
hozgurdogan <hasanmehmet84@gmail.com>
2024-01-01 20:34:11 +0300

    YonlendirmeVeModelDevami

---
+++ RezervasyonUcak/wwwroot/js/ucus.js
@@ -0,0 +1,34 @@
+﻿
+
+
+console.log("sadsadasdas");
+
+document.getElementById("sendButton").addEventListener("click", function () {
+	console.log("sadsadasdas");
+
+	var selectedId = document.getElementById("flightId").value;
+	var selectedDate = document.getElementById("flightDate").value;
+	console.log(selectedId);
+
+
+
+
+	fetch(`https://localhost:7004/Employees/Musteri/UcusGetir?konumId=${selectedId}&tarih_=${selectedDate}`)
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error('Hata:', error))
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
